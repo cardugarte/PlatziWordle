8d69d5c87fde24673cb2c5b33d81d75df3673e33
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-09-29 14:45:03 -0300

    Se agrega la lista de palabras

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import { fromEvent } from "rxjs"
+import { fromEvent, Subject } from 'rxjs'
+import WORDS_LIST from './wordList.json'
 
 const lettersRow = document.getElementsByClassName('letters-row')
 const onKeyDown$ = fromEvent(document, 'keydown')
 let letterIndex = 0
 let letterRowIndex = 0
+let userAnswer = []
+const getRandomWord = () => WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)]
+let rightWord = getRandomWord()
+const userWinOrLoser$ = new Subject
 
 
 const insertLetter = {
   next: (event) => {
-    const pressedKey = event.key
-    console.log(event.key)
+    const pressedKey = event.key.toUpperCase()
     if( pressedKey.length === 1 && pressedKey.match(/[a-z]/i )) {
       let letterBox = Array.from(lettersRow)[letterRowIndex].children[letterIndex]
       letterBox.textContent = pressedKey
       letterBox.classList.add('filled-letter')
+      userAnswer.push(pressedKey)
       letterIndex++
-      console.log(letterBox)
-    } else if ( pressedKey === 'Backspace') {
+    } else if ( event.key === 'Backspace') {
       letterIndex--
         let letterBox = Array.from(lettersRow)[letterRowIndex].children[letterIndex]
         letterBox.textContent = ''
@@ -25,6 +29,21 @@ const insertLetter = {
   }
 }
 
+const checkWord = {
+  next: (event) => {
+    if( event.key === 'Enter') {
+      if( userAnswer.join('') === rightWord ) {
+        userWinOrLoser$.next('win')
+      }
+    }
+  }
+}
+
 onKeyDown$.subscribe(insertLetter)
+onKeyDown$.subscribe(checkWord)
+userWinOrLoser$.subscribe((value) => {
+  let letterRowsWinner = Array.from(lettersRow)[letterRowIndex]
+  console.log(letterRowsWinner)
+})
 
 
