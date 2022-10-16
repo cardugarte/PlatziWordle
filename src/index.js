d14d942383b0f6e408b83d6a12d97b7bb80c4247
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-10-16 11:43:03 -0300

    Se realizan validaciones de letras

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import { fromEvent, Subject } from 'rxjs'
 import WORDS_LIST from './wordList.json'
 
-const lettersRow = document.getElementsByClassName('letters-row')
+const letterRows = document.getElementsByClassName('letters-row')
 const onKeyDown$ = fromEvent(document, 'keydown')
 let letterIndex = 0
 let letterRowIndex = 0
 let userAnswer = []
 const getRandomWord = () => WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)]
 let rightWord = getRandomWord()
+console.log(rightWord)
 const userWinOrLoser$ = new Subject
+const message = document.getElementById('message')
 
 
 const insertLetter = {
   next: (event) => {
     const pressedKey = event.key.toUpperCase()
     if( pressedKey.length === 1 && pressedKey.match(/[a-z]/i )) {
-      let letterBox = Array.from(lettersRow)[letterRowIndex].children[letterIndex]
+      let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex]
       letterBox.textContent = pressedKey
       letterBox.classList.add('filled-letter')
       userAnswer.push(pressedKey)
       letterIndex++
-    } else if ( event.key === 'Backspace') {
-      letterIndex--
-        let letterBox = Array.from(lettersRow)[letterRowIndex].children[letterIndex]
-        letterBox.textContent = ''
-        letterBox.classList.remove('filled-letter')
     }
   }
 }
@@ -32,18 +29,71 @@ const insertLetter = {
 const checkWord = {
   next: (event) => {
     if( event.key === 'Enter') {
-      if( userAnswer.join('') === rightWord ) {
-        userWinOrLoser$.next('win')
+      const rightWordArray = Array.from(rightWord)
+
+      if( userAnswer.length !== 5){
+        message.textContent = 'Te faltan algunas letras!'
+        return
+      } else {
+        message.textContent = ''
       }
+
+      for (let i = 0; i < 5; i++) {
+        let letterColor = ''
+        let letterBox = Array.from(letterRows)[letterRowIndex].children[i]
+        console.log(letterBox)
+        let letterPosition = Array.from(rightWord).indexOf(userAnswer[i])
+        console.log(letterPosition)
+
+        if(letterPosition === -1) {
+          letterColor = 'letter-grey'
+        } else {
+          if(rightWordArray[i] === userAnswer[i]) {
+            letterColor = 'letter-green'
+          } else {
+            letterColor = 'letter-yellow'
+          }
+        }
+        letterBox.classList.add(letterColor)
+      }
+      
+      // if(userAnswer.length === 5) {
+      //   letterIndex = 0
+      //   userAnswer = []
+      //   letterRowIndex ++
+      // } else {
+      //   message.textContent = 'Te faltan algunas letras!'
+      // }
+
+
+      if( userAnswer.join('') === rightWord ) {
+        userWinOrLoser$.next()
+      }
+    }
+  }
+}
+
+const removeLetter = {
+  next: (event) => {
+    const pressedKey = event.key
+    if(pressedKey === 'Backspace' && letterIndex !== 0) {
+      let letterBox = letterRows[letterRowIndex].children[userAnswer.length - 1]
+      letterBox.textContent = ''
+      letterBox.classList = 'letters'
+      letterIndex --
+      userAnswer.pop()
     }
   }
 }
 
 onKeyDown$.subscribe(insertLetter)
 onKeyDown$.subscribe(checkWord)
-userWinOrLoser$.subscribe((value) => {
-  let letterRowsWinner = Array.from(lettersRow)[letterRowIndex]
-  console.log(letterRowsWinner)
+onKeyDown$.subscribe(removeLetter)
+userWinOrLoser$.subscribe(() => {
+  let letterRowsWinned = Array.from(letterRows)[letterRowIndex]
+  for (let i = 0; i < 5; i++) {
+    letterRowsWinned.children[i].classList.add('letter-green')   
+  }
 })
 
 
